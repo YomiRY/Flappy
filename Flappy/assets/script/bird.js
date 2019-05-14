624109b6768b13b260f9204d7d80719d1505f4ad
Randy_Hsu <o1984531@gmail.com>
2019-05-14 18:07:12 +0800

    [Modify] Adjust the bird action so that it can FLY and DROP

--- Flappy/assets/script/bird.js
+++ Flappy/assets/script/bird.js
@@ -7,19 +7,20 @@
 // Learn life-cycle callbacks:
 //  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+const BIRD_STATE = {
+    FLY: 0,
+    DROP: 1,
+    DIE: 2
+};
 
 cc.Class({
     extends: cc.Component,
 
     properties: {
-    },
-
-    ctor() {
-        this.BIRD_STATE = {
-            FLY: 0,
-            DROP: 1,
-            DIE: 2
-        };
+        jumpDuration: 0.2,
+        jumpHeight: 50,
+        dropSpeed: 2,
+        state: BIRD_STATE.DROP,
     },
 
     setInputControl() {
@@ -39,11 +40,25 @@ cc.Class({
     },
 
     fly() {
-        console.log("bird flying");
+        this.state = BIRD_STATE.FLY;
+        let jumpAction = cc.sequence(
+            cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)),
+            cc.callFunc((target) => {
+                this.state = BIRD_STATE.DROP
+            }, this)
+        );
+
+        this.node.runAction(jumpAction);
+    },
+
+    drop() {
+        let node = this.node;
+        let dropAction = cc.moveBy(this.jumpDuration, cc.v2(0, -this.dropSpeed));
+        
+        node.runAction(dropAction);
     },
 
     // LIFE-CYCLE CALLBACKS:
-
     onLoad() {
         this.setInputControl();
     },
@@ -52,5 +67,9 @@ cc.Class({
 
     },
 
-    // update (dt) {},
+    update (dt) {
+        if (this.state === BIRD_STATE.DROP) {
+            this.drop();
+        }
+    },
 });
